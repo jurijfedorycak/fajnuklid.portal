2cb962eddae369de8bf9134d56b777c513838c78
Simon Skotnicky <simon.skotnicky@memos.cz>
2026-04-17 12:13:19 +0200

    fixed DB connection env

--- api/phinx.php
+++ api/phinx.php
@@ -12,6 +12,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
+$dbHost = getenv('DB_HOST');
+$dbName = getenv('DB_DATABASE');
+$dbUser = getenv('DB_USERNAME');
+$dbPass = getenv('DB_PASSWORD');
+$dbPort = getenv('DB_PORT');
+
 return [
     'paths' => [
         'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
@@ -22,31 +28,31 @@ return [
         'default_environment' => 'development',
         'production' => [
             'adapter' => 'mysql',
-            'host' => $_ENV['DB_HOST'] ?? 'localhost',
-            'name' => $_ENV['DB_DATABASE'] ?? 'fajnuklid_portal',
-            'user' => $_ENV['DB_USERNAME'] ?? 'root',
-            'pass' => $_ENV['DB_PASSWORD'] ?? '',
-            'port' => $_ENV['DB_PORT'] ?? 3306,
+            'host' => $dbHost,
+            'name' => $dbName,
+            'user' => $dbUser,
+            'pass' => $dbPass,
+            'port' => $dbPort,
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
         ],
         'development' => [
             'adapter' => 'mysql',
-            'host' => $_ENV['DB_HOST'] ?? 'localhost',
-            'name' => $_ENV['DB_DATABASE'] ?? 'fajnuklid_portal',
-            'user' => $_ENV['DB_USERNAME'] ?? 'root',
-            'pass' => $_ENV['DB_PASSWORD'] ?? '',
-            'port' => $_ENV['DB_PORT'] ?? 3306,
+            'host' => $dbHost,
+            'name' => $dbName,
+            'user' => $dbUser,
+            'pass' => $dbPass,
+            'port' => $dbPort,
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
         ],
         'testing' => [
             'adapter' => 'mysql',
-            'host' => $_ENV['DB_HOST'] ?? 'localhost',
-            'name' => ($_ENV['DB_DATABASE'] ?? 'fajnuklid_portal') . '_test',
-            'user' => $_ENV['DB_USERNAME'] ?? 'root',
-            'pass' => $_ENV['DB_PASSWORD'] ?? '',
-            'port' => $_ENV['DB_PORT'] ?? 3306,
+            'host' => $dbHost,
+            'name' => $dbName . '_test',
+            'user' => $dbUser,
+            'pass' => $dbPass,
+            'port' => $dbPort,
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
         ],
