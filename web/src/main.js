470caecc8a1caefa786864cf02fb0210e184b0aa
Simon Skotnicky <simon.skotnicky@memos.cz>
2026-05-16 14:37:02 +0200

    fixed redirection to login page if unauthorized

--- web/src/main.js
+++ web/src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { useAuth } from './stores/auth'
 
-createApp(App).use(router).mount('#app')
+async function bootstrap() {
+  const { isAuthenticated, checkAuth } = useAuth()
+  if (isAuthenticated.value) {
+    await checkAuth()
+  }
+  createApp(App).use(router).mount('#app')
+}
+
+bootstrap()
